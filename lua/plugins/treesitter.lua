ea16d51cf77ccf7015bf10f4b322ffc29e6c7c5a
LiorPeless <lior.peles@gmail.com>
2024-03-16 10:27:34 +0200

    added formating for lua and python

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,14 @@
-return { 
-	"nvim-treesitter/nvim-treesitter", 
+return {
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	
+
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "python"},
+            auto_install = true,
+			ensure_installed = { "lua", "python" },
 			highlight = { enable = true },
-        		indent = { enable = true },  
-
-			})
-	end
-
-
+			indent = { enable = true },
+		})
+	end,
 }
-
