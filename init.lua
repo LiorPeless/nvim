b6c0d8469f3cef66d0fc41ace6ef4ccd663a3a6b
LiorPeless <lior.peles@gmail.com>
2024-03-16 22:47:53 +0200

    added line numbers

--- init.lua
+++ init.lua
@@ -4,17 +4,19 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 -- Use spaces instead of tabs
 vim.opt.expandtab = true
+-- Enable line numbers
+vim.cmd("set number")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
