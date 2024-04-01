66fcd3d0e9290465336690e91a43c8864e15ec36
LiorPeless <lior.peles@gmail.com>
2024-04-01 21:07:33 +0300

    a big commit: added a dashboard with Cirno, and more telescope stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,10 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>cs", builtin.colorscheme, {})
+            vim.keymap.set("n", "<leader>ff", builtin.current_buffer_fuzzy_find, {})
         end,
     },
     {
